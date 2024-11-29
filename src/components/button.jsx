f91505af72706f0947e881236985d6ad45feb339
NITESH BHARDWAJ <niteshbhardwaj2001@gamil.com>
2024-11-29 19:11:17 +0530

    buttons default props passing,changes

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,15 @@
 import { IoIosReturnRight } from "react-icons/io";
-function Button() {
+function Button({title="get Started"}) {
   return (
     <>
       <div className="flex items-center justify-evenly but w-40 px-4 py-2 bg-zinc-100 rounded-full ">
-        <span className="text-sm font-medium">Get Started</span>
+        <span className="text-sm font-medium">{title}</span>
         <IoIosReturnRight />
       </div>
     </>
   );
 }
-
+// Button.defaultProps = {
+//   title: "Get Started"
+// }
 export default Button;
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -8,8 +8,14 @@ function Product({ data }) {
         <h1 className="text-6xl capitalize font-semibold">{data.title}</h1>
         <div className="w-1/3">
           <p className="mb-10 ">{data.description}</p>
-          <div className="text-black">
-            <Button />
+          <div className="text-black flex gap-10">
+            {
+              data.live && (<Button/>)
+              
+            }
+            {
+              data.case && (<Button title="Case Study"/>)
+            }
           </div>
         </div>
       </div>
