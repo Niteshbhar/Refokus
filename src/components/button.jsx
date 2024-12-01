6685db630fb5d4839776639d6bb234c98155c923
NITESH BHARDWAJ <niteshbhardwaj2001@gamil.com>
2024-12-01 19:03:32 +0530

    button hover effect added

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,11 +1,18 @@
 import { IoIosReturnRight } from "react-icons/io";
-function Button({title="get Started"}) {
+function Button({ title = "get Started" }) {
   return (
     <>
-      <div className="flex items-center justify-evenly but w-40 px-4 py-2 bg-zinc-100 rounded-full ">
-        <span className="text-sm font-medium">{title}</span>
-        <IoIosReturnRight />
-      </div>
+      <div className="overflow-hidden rounded-full flex-col w-[12vw] h-8 relative group">
+  <div className="absolute flex items-center justify-evenly w-full px-4 py-2 bg-zinc-100 rounded-full h-8 effects cursor-pointer">
+    <span className="text-sm font-medium">{title}</span>
+    <IoIosReturnRight />
+  </div>
+  <div className="absolute flex items-center justify-evenly w-full px-4 py-2 bg-zinc-100 rounded-full h-8 cursor-pointer effects">
+    <span className="text-sm font-medium">{title}</span>
+    <IoIosReturnRight />
+  </div>
+</div>
+
     </>
   );
 }
